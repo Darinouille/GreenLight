918518efe2e96210c460db89a519258f32dd0889
Darina <76685823+Darinouille@users.noreply.github.com>
2023-01-16 18:50:58 +0100

    Update Jour.php

--- php/Jour.php
+++ php/Jour.php
@@ -1,28 +1,9 @@
-
 <?php
-
 include("connexion.php");
-try {
-    $bd = new PDO ( "mysql:host = $server; dbname=$base", "$user", "$password"); 
-    $bd->exec("SET NAMES utf8");
-    //echo("Connexion à la base de donnée réussie, bien joué");
-}
-catch (Exception $e){
-    print_r($e -> getMessage());
-    die("Erreur de connexion à la base ");
-}
 
+// Les données du jour 0
 $jour0 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=0" );
 $monjour0 = ($jour0->fetch(PDO::FETCH_ASSOC));
-
-$jour1 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=1" );
-$monjour1 = ($jour1->fetch(PDO::FETCH_ASSOC));
-
-$jour2 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=2" );
-$monjour2 = ($jour2->fetch(PDO::FETCH_ASSOC));
-
-$jour3 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=3" );
-$monjour3 = ($jour3->fetch(PDO::FETCH_ASSOC));
 
 $j0 = array(
     "jour: " => $monjour0['jour'],
@@ -31,6 +12,11 @@ $j0 = array(
     "message: " => $monjour0['message'],
 );
 
+
+// Les données du jour 1
+$jour1 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=1" );
+$monjour1 = ($jour1->fetch(PDO::FETCH_ASSOC));
+
 $j1 = array(
     "jour: " => $monjour1['jour'],
     "dvalue: " => $monjour1['#dvalue'],
@@ -38,12 +24,22 @@ $j1 = array(
     "message: " => $monjour1['message'],
 );
 
+
+// Les données du jour 2
+$jour2 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=2" );
+$monjour2 = ($jour2->fetch(PDO::FETCH_ASSOC));
+
 $j2 = array(
     "jour: " => $monjour2['jour'],
     "dvalue: " => $monjour0['#dvalue'],
     "generation fichier: " => $monjour2['generationFichier'],
     "message: " => $monjour2['message'],
 );
+
+
+// Les données du jour 3
+$jour3 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=3" );
+$monjour3 = ($jour3->fetch(PDO::FETCH_ASSOC));
 
 $j3 = array(
     "jour: " => $monjour3['jour'],
@@ -53,22 +49,16 @@ $j3 = array(
 );
 
 
-
-
+// Mise en forme
 $tableau = array(
     "Jour0: " => $j0,
     "Jour1: " => $j1,
     "Jour2: " => $j2,
     "Jour3: " => $j3,
-
 );
 
-//print_r($j0);
-
-
+// Transformation en JSON
 echo json_encode($tableau);
-
-
 ?>
 
 
