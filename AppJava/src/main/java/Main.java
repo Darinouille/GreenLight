49aefbdfd5f10ad875ee2ff8348f74da66fef20b
Darina <76685823+Darinouille@users.noreply.github.com>
2023-01-16 01:02:44 +0100

    Update Main.java

--- AppJava/src/main/java/Main.java
+++ AppJava/src/main/java/Main.java
@@ -3,6 +3,17 @@ import java.sql.SQLException;
 import org.json.*;
 
 public class Main {
+    public static String convertirDate(String date1) {
+        String date2;
+        // On a quelque chose sous la forme : yyyy-mm-jj-Thh:mm:ss+02:zz
+        //                                    01234567890123456789012345
+
+        // On souhaite avoir une forme : yyyy-mm-jj hh:mm:ss.zzzzzz
+
+        date2 = date1.substring(0, 10) + " " + date1.substring(11, 19) + "." + date1.substring(23, 25) + "0000";
+        return date2;
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
         Lecture l;
         l = new Lecture();
@@ -14,7 +25,6 @@ public class Main {
         
         // On se connecte à la base de données pour écrire
         Ecriture e;
-        String jour;
         int pos;
 
         e = new Ecriture();
@@ -25,17 +35,17 @@ public class Main {
         int ordreJour[] = {3, 1, 2, 0};
 
         for (int j : ordreJour) {
+            // On récupère la date de génération du fichier : generationFichier
+            String dateF = json.getJSONArray("signals").getJSONObject(pos).getString("GenerationFichier");
+            String generationFichier = convertirDate(dateF);
+
             // On récupère la valeur du jour j : dvalue
             int dvalue = json.getJSONArray("signals").getJSONObject(pos).getInt("dvalue");
             // Le timestamp du jour
-            // On a quelque chose sous la forme : yyyy-mm-jj-Thh:mm:ss+02:zz
-            //                                    01234567890123456789012345
-            // On souhaite avoir une forme : yyyy-mm-jj hh:mm:ss.zzzzzz
-
             String date = json.getJSONArray("signals").getJSONObject(pos).getString("jour");
-            jour = date.substring(0, 10) + " " + date.substring(11, 19) + "." + date.substring(23, 25) + "0000";
+            String jour = convertirDate(date);
 
-            e.insertJour(j, jour, dvalue);
+            e.insertJour(j, jour, generationFichier, dvalue);
 
             System.out.println("\n✽ J+" + j + " (" + jour + ") | valeur : " + dvalue);
             System.out.println("Voici les données pour chaque heure de ce jour");
@@ -50,6 +60,8 @@ public class Main {
 
                 System.out.println("• " + h + "h : " + hvalue);
             }
+
+            System.out.println("La génération du fichier date du : " + generationFichier);
             pos = pos + 1;
         }
     }
