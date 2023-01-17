68af52c975f4ec7a9f4f266ffa7be8ca41f817ac
Darina <76685823+Darinouille@users.noreply.github.com>
2023-01-17 23:32:06 +0100

    Add files via upload

---
+++ php/Heure.php
@@ -0,0 +1,74 @@
+<?php
+
+include("connexion.php");
+
+// Les données heures du jour 0
+$sql = "SELECT Heure.pas, Heure.hvalue FROM Jour LEFT JOIN Heure ON Jour.idJour=Heure.`#idjour` WHERE Jour.idJour=0";
+$req = $bd ->prepare($sql);
+$req->execute();
+$heures0 = $req->fetchall();
+$req->closeCursor();
+
+$hjour0 = array("pas" => "hvalue");
+foreach($heures0 as $h) {
+  $pas = $h['pas'];
+  $hvalue = $h['hvalue'];
+  $hjour0[$pas] = $hvalue;
+}
+
+
+// Les données heures du jour 1
+$sql = "SELECT Heure.pas, Heure.hvalue FROM Jour LEFT JOIN Heure ON Jour.idJour=Heure.`#idjour` WHERE Jour.idJour=1";
+$req = $bd ->prepare($sql);
+$req->execute();
+$heures1 = $req->fetchall();
+$req->closeCursor();
+
+$hjour1 = array("pas" => "hvalue");
+foreach($heures1 as $h) {
+  $pas = $h['pas'];
+  $hvalue = $h['hvalue'];
+  $hjour1[$pas] = $hvalue;
+}
+
+
+// Les données heures du jour 2
+$sql = "SELECT Heure.pas, Heure.hvalue FROM Jour LEFT JOIN Heure ON Jour.idJour=Heure.`#idjour` WHERE Jour.idJour=2";
+$req = $bd ->prepare($sql);
+$req->execute();
+$heures2 = $req->fetchall();
+$req->closeCursor();
+
+$hjour2 = array("pas" => "hvalue");
+foreach($heures2 as $h) {
+  $pas = $h['pas'];
+  $hvalue = $h['hvalue'];
+  $hjour2[$pas] = $hvalue;
+}
+
+
+// Les données heures du jour 3
+$sql = "SELECT Heure.pas, Heure.hvalue FROM Jour LEFT JOIN Heure ON Jour.idJour=Heure.`#idjour` WHERE Jour.idJour=3";
+$req = $bd ->prepare($sql);
+$req->execute();
+$heures3 = $req->fetchall();
+$req->closeCursor();
+
+$hjour3 = array("pas" => "hvalue");
+foreach($heures3 as $h) {
+  $pas = $h['pas'];
+  $hvalue = $h['hvalue'];
+  $hjour3[$pas] = $hvalue;
+}
+
+
+// Mise en forme des informations
+$tableau = array(
+  "Jour0: " => $hjour0,
+  "Jour1: " => $hjour1,
+  "Jour2: " => $hjour2,
+  "Jour3: " => $hjour3);
+
+// Transformation en json
+echo json_encode($tableau);
+?>
---
+++ php/Jour.php
@@ -0,0 +1,62 @@
+<?php
+include("connexion.php");
+
+// Les données du jour 0
+$jour0 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=0" );
+$monjour0 = ($jour0->fetch(PDO::FETCH_ASSOC));
+
+$j0 = array(
+    "jour: " => $monjour0['jour'],
+    "dvalue: " => $monjour0['#dvalue'],
+    "generation fichier: " => $monjour0['generationFichier'],
+    "message: " => $monjour0['message'],
+);
+
+
+// Les données du jour 1
+$jour1 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=1" );
+$monjour1 = ($jour1->fetch(PDO::FETCH_ASSOC));
+
+$j1 = array(
+    "jour: " => $monjour1['jour'],
+    "dvalue: " => $monjour1['#dvalue'],
+    "generation fichier: " => $monjour1['generationFichier'],
+    "message: " => $monjour1['message'],
+);
+
+
+// Les données du jour 2
+$jour2 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=2" );
+$monjour2 = ($jour2->fetch(PDO::FETCH_ASSOC));
+
+$j2 = array(
+    "jour: " => $monjour2['jour'],
+    "dvalue: " => $monjour0['#dvalue'],
+    "generation fichier: " => $monjour2['generationFichier'],
+    "message: " => $monjour2['message'],
+);
+
+
+// Les données du jour 3
+$jour3 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generationFichier,Message.message FROM Jour  LEFT JOIN Message ON Jour.`#dvalue`=Message.dValue  WHERE Jour.idJour=3" );
+$monjour3 = ($jour3->fetch(PDO::FETCH_ASSOC));
+
+$j3 = array(
+    "jour: " => $monjour3['jour'],
+    "dvalue: " => $monjour3['#dvalue'],
+    "generation fichier: " => $monjour3['generationFichier'],
+    "message: " => $monjour3['message'],
+);
+
+
+// Mise en forme
+$tableau = array(
+    "Jour0: " => $j0,
+    "Jour1: " => $j1,
+    "Jour2: " => $j2,
+    "Jour3: " => $j3,
+);
+
+// Transformation en JSON
+echo json_encode($tableau);
+?>
