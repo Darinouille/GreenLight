a45eaabaa85c92024a67f45b08e2d9bd40faa7f4
Darina <76685823+Darinouille@users.noreply.github.com>
2023-01-18 01:01:15 +0100

    Update Jour.php

--- php/Jour.php
+++ php/Jour.php
@@ -6,10 +6,10 @@ $jour0 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generatio
 $monjour0 = ($jour0->fetch(PDO::FETCH_ASSOC));
 
 $j0 = array(
-    "jour: " => $monjour0['jour'],
-    "dvalue: " => $monjour0['#dvalue'],
-    "generation fichier: " => $monjour0['generationFichier'],
-    "message: " => $monjour0['message'],
+    "jour" => $monjour0['jour'],
+    "dvalue" => $monjour0['#dvalue'],
+    "generation fichier" => $monjour0['generationFichier'],
+    "message" => $monjour0['message'],
 );
 
 
@@ -18,10 +18,10 @@ $jour1 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generatio
 $monjour1 = ($jour1->fetch(PDO::FETCH_ASSOC));
 
 $j1 = array(
-    "jour: " => $monjour1['jour'],
-    "dvalue: " => $monjour1['#dvalue'],
-    "generation fichier: " => $monjour1['generationFichier'],
-    "message: " => $monjour1['message'],
+    "jour" => $monjour1['jour'],
+    "dvalue" => $monjour1['#dvalue'],
+    "generation fichier" => $monjour1['generationFichier'],
+    "message" => $monjour1['message'],
 );
 
 
@@ -30,10 +30,10 @@ $jour2 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generatio
 $monjour2 = ($jour2->fetch(PDO::FETCH_ASSOC));
 
 $j2 = array(
-    "jour: " => $monjour2['jour'],
-    "dvalue: " => $monjour2['#dvalue'],
-    "generation fichier: " => $monjour2['generationFichier'],
-    "message: " => $monjour2['message'],
+    "jour" => $monjour2['jour'],
+    "dvalue" => $monjour2['#dvalue'],
+    "generation fichier" => $monjour2['generationFichier'],
+    "message" => $monjour2['message'],
 );
 
 
@@ -42,19 +42,19 @@ $jour3 = $bd ->query("SELECT Jour.jour,Jour.idJour,Jour.`#dvalue`,Jour.generatio
 $monjour3 = ($jour3->fetch(PDO::FETCH_ASSOC));
 
 $j3 = array(
-    "jour: " => $monjour3['jour'],
-    "dvalue: " => $monjour3['#dvalue'],
-    "generation fichier: " => $monjour3['generationFichier'],
-    "message: " => $monjour3['message'],
+    "jour" => $monjour3['jour'],
+    "dvalue" => $monjour3['#dvalue'],
+    "generation fichier" => $monjour3['generationFichier'],
+    "message" => $monjour3['message'],
 );
 
 
 // Mise en forme
 $tableau = array(
-    "Jour0: " => $j0,
-    "Jour1: " => $j1,
-    "Jour2: " => $j2,
-    "Jour3: " => $j3,
+    "Jour0" => $j0,
+    "Jour1" => $j1,
+    "Jour2" => $j2,
+    "Jour3" => $j3,
 );
 
 // Transformation en JSON
